1dbb7983dc2b6916e3081f8e47aba4c925505dce
njy1731 <77655353+njy1731@users.noreply.github.com>
2022-11-29 14:00:48 +0900

    prefab upload

---
+++ Assets/Scripts/A.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("D");
+    }
+
+    void Update()
+    {
+        
+    }
+}
