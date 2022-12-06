b4373e5d584026f7c4b52424c0edfe33ac50eb8f
jehyeongju <bjnotch@naver.com>
2022-12-06 20:28:15 +0900

    수납장 열림

--- Assets/Scripts/ClosetCtrl.cs
+++ Assets/Scripts/ClosetCtrl.cs
@@ -4,16 +4,67 @@ using UnityEngine;
 
 public class ClosetCtrl : MonoBehaviour
 {
-    private GameObject HidePoint;
-    private Vector3 pointTarget;
+    public Transform PlayerCamera;
+    [Header("MaxDistance you can open or close the door.")]
+    public float MaxDistance = 5;
+    private GameObject playerCamera;
+    private bool insideCloset;
+    private GameObject player;
+    [SerializeField]
+    private GameObject hidePoint;
+    private bool opened = false;
+    private Animator anim;
+
     void Start()
     {
-        pointTarget = HidePoint.gameObject.transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+        hidePoint = GameObject.FindGameObjectWithTag("HidePoint");
+        playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        PlayerCamera = playerCamera.transform;
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Pressed();
+            
+        }
     }
+
+    void Pressed()
+    {
+        RaycastHit doorhit;
+
+        if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out doorhit, MaxDistance))
+        {
+
+            if (doorhit.transform.tag == "ClosetDoor")
+            {
+                anim = doorhit.transform.GetComponentInParent<Animator>();
+
+                opened = !opened;
+
+                anim.SetBool("Opened", !opened);
+
+                player.transform.position = hidePoint.transform.position;
+                Debug.Log("Teleport");
+                
+
+                Invoke("CloseDoor", 1f);
+
+                insideCloset = true;
+
+                
+            }
+        }
+    }
+    void CloseDoor()
+    {
+        anim.SetBool("Opened", false);
+    }
+
 }
