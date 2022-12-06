47ebd6993fb345c0bac273a8832aed7779a8e66d
njy1731 <77655353+njy1731@users.noreply.github.com>
2022-12-06 16:35:09 +0900

    퍼즐 만들기 시작...난죽고말거야

---
+++ Assets/Scripts/Room/PuzzleNumber.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class PuzzleNumber : MonoBehaviour
+{
+    private int[] puzzlePicture;
+    private int id = 0;
+
+    //string RandomStringGenerator(int length)
+    //{
+    //    string password = "0123456789";
+    //    string password_string = "";
+
+    //    for (int i = 0; i < length; i++)
+    //        password_string += password[UnityEngine.Random.Range(0, length)];
+
+    //    return password_string;
+    //}
+}
---
+++ Assets/Scripts/Room/PuzzleSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Puzzle", menuName ="Room/Puzzles/New Puzzle")]
+public class PuzzleSO : ScriptableObject
+{
+    
+}
