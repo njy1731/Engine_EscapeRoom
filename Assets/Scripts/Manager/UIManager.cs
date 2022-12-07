112ea302c82a1b59952453f7ff166a475c01f6e6
njy1731 <77655353+njy1731@users.noreply.github.com>
2022-12-08 01:18:06 +0900

    .

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Text Info")]
+    [SerializeField] private Text getItemText;
+    [SerializeField] private Text getOpenFurnitureText;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
--- Assets/Scripts/Room/PuzzleCtrl.cs
+++ Assets/Scripts/Room/PuzzleCtrl.cs
@@ -5,15 +5,67 @@ using UnityEngine.UI;
 
 public class PuzzleCtrl : MonoBehaviour
 {
-
+    //[SerializeField] private PuzzleSO puzzle;
+    [SerializeField] private int maxCount;
+    [SerializeField] private int n;
+    private string name_ = "";
 
     void Start()
     {
         
+        //puzzle.RandomNum(10);
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if (name_ == "")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                RandomPassword();
+            }
+        }
+
+        else
+        {
+            name_ = "";
+        }
+    }
+
+    private void RandomPassword()
+    {
+        int[] numbers = PasswordGenerator.RandomNumbers(maxCount, n);
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            name_ += numbers[i].ToString();
+        }
+        Debug.Log(name_);
+    }
+
+    public static class PasswordGenerator
+    {
+        public static int[] RandomNumbers(int maxCount, int n)
+        {
+            int[] defaults = new int[maxCount];
+            int[] results = new int[n];
+
+            for (int i = 0; i < maxCount; ++i)
+            {
+                defaults[i] = i;
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                int index = Random.Range(0, maxCount);
+
+                results[i] = defaults[index];
+                defaults[index] = defaults[maxCount - 1];
+
+                maxCount--;
+            }
+
+            return results;
+        }
     }
 }
+
