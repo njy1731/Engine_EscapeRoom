4d4cf89f2bf675f65d558c77ddb93c0221f37eea
jehyeongju <bjnotch@naver.com>
2022-12-15 01:51:10 +0900

    enemyScare

---
+++ Assets/Scripts/Enemy/deathSceneCtrl.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class deathSceneCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image enemyImage;
+    public Image enemyScaryImage;
+    public Image redbackGround;
+    public Image whitebackGround;
+
+    void Start()
+    {
+        
+        Invoke("ChangeImage", 2f);
+        Invoke("FirstStep", 2.3f);
+        Invoke("SecondStep", 2.35f);
+        Invoke("ThirdStep", 2.4f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FirstStep()
+    {
+        redbackGround.gameObject.SetActive(true);
+        enemyScaryImage.rectTransform.localScale = new Vector3(2f, 2f);
+    }
+    void SecondStep()
+    {
+        whitebackGround.gameObject.SetActive(true);
+        enemyScaryImage.rectTransform.localScale = new Vector3(3f, 3f);
+    }
+    void ThirdStep()
+    {
+        whitebackGround.gameObject.SetActive(false);
+        enemyScaryImage.rectTransform.localScale = new Vector3(4f, 4f);
+    }
+    void ChangeImage()
+    {
+        enemyImage.gameObject.SetActive(false);
+        enemyScaryImage.gameObject.SetActive(true);
+    }
+}
