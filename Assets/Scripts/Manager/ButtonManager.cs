8f11bbd02896508f15f551c1b28b6d5aaa50046d
njy1731 <77655353+njy1731@users.noreply.github.com>
2022-12-21 20:14:12 +0900

    .

--- Assets/Scripts/Manager/ButtonManager.cs
+++ Assets/Scripts/Manager/ButtonManager.cs
@@ -1,25 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    [Header("UI")]
+    [SerializeField] private GameObject UserGuideUI;
+    [SerializeField] private GameObject StartUI;
+    [SerializeField] private GameObject FirstPage;
+    [SerializeField] private GameObject SecondPage;
+    [SerializeField] private GameObject ExitGuideButton;
+    [SerializeField] private GameObject NextPageButton;
+    [SerializeField] private GameObject PreviousPageButton;
+
     public void StartButton()
     {
         SceneManager.LoadScene("elevtorScene");
     }
-    public void OptionButton()
-    {
 
-    }
     public void ExitButton()
     {
         Application.Quit();
     }
+
     public void MenuButton()
     {
         SceneManager.LoadScene("GameStartScene");
     }
- 
+
+    public void OnHowToPlayButtonDown()
+    {
+        UserGuideUI.SetActive(true);
+        StartUI.SetActive(false);
+        FirstPage.SetActive(true);
+        SecondPage.SetActive(false);
+        NextPageButton.SetActive(true);
+        PreviousPageButton.SetActive(false);
+    }
+
+    public void ExitGuideButtonDown()
+    {
+        UserGuideUI.SetActive(false);
+        StartUI.SetActive(true);
+        NextPageButton.SetActive(false);
+        PreviousPageButton.SetActive(false);
+    }
+
+    public void OnNextPageButtonDown()
+    {
+        FirstPage.SetActive(false);
+        SecondPage.SetActive(true);
+        NextPageButton.SetActive(false);
+        PreviousPageButton.SetActive(true);
+    }
+
+    public void OnPreviousPageButtonDown()
+    {
+        FirstPage.SetActive(true);
+        SecondPage.SetActive(false);
+        NextPageButton.SetActive(true);
+        PreviousPageButton.SetActive(false);
+    }
 }
