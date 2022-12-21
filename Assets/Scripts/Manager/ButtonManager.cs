b6f83d8511f33c715b740f17b2f9383cc516c34c
njy1731 <77655353+njy1731@users.noreply.github.com>
2022-12-21 20:29:53 +0900

    .

--- Assets/Scripts/Manager/ButtonManager.cs
+++ Assets/Scripts/Manager/ButtonManager.cs
@@ -6,15 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    [Header("UI")]
-    [SerializeField] private GameObject UserGuideUI;
-    [SerializeField] private GameObject StartUI;
-    [SerializeField] private GameObject FirstPage;
-    [SerializeField] private GameObject SecondPage;
-    [SerializeField] private GameObject ExitGuideButton;
-    [SerializeField] private GameObject NextPageButton;
-    [SerializeField] private GameObject PreviousPageButton;
-
     public void StartButton()
     {
         SceneManager.LoadScene("elevtorScene");
@@ -28,39 +19,5 @@ public class ButtonManager : MonoBehaviour
     public void MenuButton()
     {
         SceneManager.LoadScene("GameStartScene");
-    }
-
-    public void OnHowToPlayButtonDown()
-    {
-        UserGuideUI.SetActive(true);
-        StartUI.SetActive(false);
-        FirstPage.SetActive(true);
-        SecondPage.SetActive(false);
-        NextPageButton.SetActive(true);
-        PreviousPageButton.SetActive(false);
-    }
-
-    public void ExitGuideButtonDown()
-    {
-        UserGuideUI.SetActive(false);
-        StartUI.SetActive(true);
-        NextPageButton.SetActive(false);
-        PreviousPageButton.SetActive(false);
-    }
-
-    public void OnNextPageButtonDown()
-    {
-        FirstPage.SetActive(false);
-        SecondPage.SetActive(true);
-        NextPageButton.SetActive(false);
-        PreviousPageButton.SetActive(true);
-    }
-
-    public void OnPreviousPageButtonDown()
-    {
-        FirstPage.SetActive(true);
-        SecondPage.SetActive(false);
-        NextPageButton.SetActive(true);
-        PreviousPageButton.SetActive(false);
     }
 }
